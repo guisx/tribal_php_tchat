bd5e2e3ae82f8c9c44a891cfcca05a2b058c19af
guisx <antpruvost@gmail.com>
2014-11-02 17:00:26 +0100

    Ajout de fonctionalites php dans tchat

--- tchat.php
+++ tchat.php
@@ -1,7 +1,11 @@
 <!-- L'en-tête -->
 
 <?php include("header.php"); ?>
-
+<?php
+    //on recupere les fichiers pour se connecte a la BDD
+    require_once 'config.php';
+    require_once 'functions.php';
+?>
 <?php 
     session_start();
     //On redirige si le pseudo n'existe pas ou est vide 
@@ -9,10 +13,19 @@
 		header("location:index.php");
 	}
 ?>
-
+<h1>Bienvenue sur Tribal tchat : <i><?php echo $_SESSION['pseudo']; ?></i></h1>
 <section>
-	<h1>Bienvenue sur Tribal tchat : <i><?php echo $_SESSION['pseudo']; ?></i></h1>
+    <?php
+        // Récupération des 10 derniers messages
+        $reponse = $bdd->query('SELECT pseudo, message, DAY(date_message) AS jour, MONTH(date_message) AS mois, YEAR(date_message) AS annee, HOUR(date_message) AS heure, MINUTE(date_message) AS minute, SECOND(date_message) AS seconde FROM minichat ORDER BY date_message DESC LIMIT 10');
 
+        // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+        while ($donnees = $reponse->fetch()){
+            echo '<p>[' . $donnees['jour'] . '/' . $donnees['mois'] . '/' . $donnees['annee'] . ' ' . $donnees['heure'] . 'h' . $donnees['minute'] . 'm' . $donnees['seconde'] . 's] <strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+        }
+        $reponse->closeCursor();
+
+    ?>
 </section>
 
 <!-- Boite pour l'envoi du message -->
